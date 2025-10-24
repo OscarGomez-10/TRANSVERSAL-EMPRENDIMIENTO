12a6190251076ffacb2aadf6c5b7565fdfa0ac7b
Oscar Gomez <oscarr.2343@gmail.com>
2025-10-24 10:23:57 -0500

    Update API base URL to production server

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/veterinaria"
+    baseURL: "https://transversal-emprendimiento-backend.onrender.com"
 })
 
 
